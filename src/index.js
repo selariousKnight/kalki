87e966dde750d24d496393af0dc9a000f0d4cae5
Pavan kumar <pavankumar98989898@gmail.com>
2024-01-14 23:49:39 +0530

    Todays commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ ReactDOM.render(
     <BrowserRouter>
     <Routes>
       <Route path = "/" element= {<App/>}/>
-      <Route path = "/about" element= {<About/>}/>
+      <Route path = "/about" element = {<About/>}/>
       <Route path = "/page" element= {<Page/>}/>
     </Routes>
     </BrowserRouter>,
